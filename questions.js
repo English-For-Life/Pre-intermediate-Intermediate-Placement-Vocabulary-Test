5c3351ee991c2018a7b4b1900117c61e0362d196
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-11 00:05:26 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,56 @@
+//Array of objects
+const quiz = [
+    {
+        q:"A lot of ____ came to Ireland in the 1990s.",
+        options:["immigrants", "emigrants", "invaders", "colonies"],
+        answer: 0
+    },
+    {
+        q:"There was a nice meal and a band at the wedding ____ .",
+        options:["ceremony", "reception", "speech", "group"],
+        answer: 1
+    },
+    {
+        q:"I mostly ____ my friends via email. ",
+        options:["see each other", "have in common ", "keep in touch with", "get on well with"],
+        answer: 2
+    },
+    {
+        q:"Bob has had a very interesting ____. He has had jobs in many countries and industries.",
+        options:["work", "job", "carrier", "career" ],
+        answer: 3
+    },
+    {
+        q:"She’s very successful. Her ____ has risen a lot in the past few years. ",
+        options:["salary", "money", "job", "earnings"],
+        answer: 0
+    },
+    {
+        q:"I am very ____ in old cars. ",
+        options:["interesting", "interested", "fond", "keen"],
+        answer: 1
+    },
+    {
+        q:"He ____ his exam because he didn’t study.",
+        options:["passed", "missed", "failed", "fell"],
+        answer: 2   
+    },
+    {
+        q:"The house will look cleaner when you have finished the ____.",
+        options:["home", "housewife", "homework", "housework"],
+        answer: 3
+    },
+    {
+        q:"Stress is not an illness, but it can ____ to many illnesses. ",
+        options:["contribute", "affect", "celebrate", "get"],
+        answer: 0
+    },
+    {
+        q:"He ____ off his holiday until after the winter.",
+        options:["took", "put", "called", "logged"],
+        answer: 1
+    }
+    
+
+
+]
